ffbaaff34e36b3752c356009ac67cb8a652a08e1
Allan Charles <allancharles95@outlook.com>
2019-06-26 17:18:25 +0100

    Sorted the run.sh to include user prompts

--- scripts/VM.sh
+++ scripts/VM.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-az vm create --resource-group MainResource --name JenkinsVM --image ubuntults --admin-username admin123 --admin-password Password123456789! --nics WildinsNICOne --size Standard_B1ms
-az vm create --resource-group MainResource --name WildflyVM --image ubuntults --admin-username admin123 --admin-password Password123456789! --nics WildinsNICTwo --size Standard_B1ms
+az vm create --resource-group MainResource --name JenkinsVM --image ubuntults --admin-username admin123 --admin-password Password123456789! --nics WildinsNICOne --size Standard_B1ms --generate-ssh-keys
+az vm create --resource-group MainResource --name WildflyVM --image ubuntults --admin-username admin123 --admin-password Password123456789! --nics WildinsNICTwo --size Standard_B1ms --generate-ssh-keys
